e3a1dc28ef0c4c1bc4756c0ccabbce6ec4356eb0
Mahardika Ramadhana <mdikaramadhana@gmail.com>
2026-06-10 15:10:45 +0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleaned;
+        for (char x : s) {
+            if(isalnum(x)) {
+                cleaned+=tolower(x);
+            }
+        }
+
+        for (int i = 0; i < (int)cleaned.length()/2; i++) {
+            if (cleaned[i] != cleaned[cleaned.length()-1-i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
